6b525855f8a25d3ea264abc23058d275bbfafd63
Jerin Philip <jerinphilip@live.in>
2023-09-16 19:11:44 +0530

    Remove automatic fastboot paths

--- main.py
+++ main.py
@@ -11,18 +11,6 @@ logging.basicConfig(level=logging.WARNING)
 
 use_fastboot = False
 
-try:
-    from adb.fastboot import FastbootCommands
-    from adb.usb_exceptions import *
-
-    fdev = FastbootCommands()
-    try:
-        fdev.ConnectDevice()
-        use_fastboot = True
-    except DeviceNotFoundError:
-        logging.warning("No device detected in fastboot. Using manual mode.")
-except ImportError:
-    logging.warning("python-adb fastboot interface unavailable. Using manual mode.")
 
 auth = Auth()
 auth.login_tui("unlockApi")
@@ -58,34 +46,17 @@ mtkqcom = input(
 )  # This is the worst way to implement this because i suck at everything
 
 if mtkqcom == "mtk":
-    if use_fastboot:
-        product = fdev.Getvar("product").decode("utf-8")
-        token = fdev.Oem("get_token").decode("utf-8")
-        logging.debug("product is %s, token is %s", product, token)
-
-    else:
-        product = input(
-            "Enter output from `fastboot getvar product` (Ctrl-C to cancel): "
-        )
-        token = input(
-            "Enter output from `fastboot oem get_token` \nCombine the strings (left to right) then enter it here. (Ctrl-C to cancel): "
-        )
-        logging.debug("product is %s, token is %s", product, token)
+    product = input("Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
+    token = input(
+        "Enter output from `fastboot oem get_token` \nCombine the strings (left to right) then enter it here. (Ctrl-C to cancel): "
+    )
+    logging.debug("product is %s, token is %s", product, token)
 
 
 elif mtkqcom == "qcom":
-    if use_fastboot:
-        product = fdev.Getvar("product").decode("utf-8")
-        token = fdev.Getvar("token").decode("utf-8")
-        logging.debug("product is %s, token is %s", product, token)
-
-    else:
-        product = input(
-            "Enter output from `fastboot getvar product` (Ctrl-C to cancel): "
-        )
-        token = input("Enter output from `fastboot getvar token` (Ctrl-C to cancel): ")
-        logging.debug("product is %s, token is %s", product, token)
-
+    product = input("Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
+    token = input("Enter output from `fastboot getvar token` (Ctrl-C to cancel): ")
+    logging.debug("product is %s, token is %s", product, token)
 
 else:
     print("Invalid answer.")
@@ -144,21 +115,5 @@ r.add_nonce()
 data = r.run()
 logging.debug(data)
 
-if use_fastboot:
-    confirm = input("Acquired token! Do you want to unlock now? Enter `yes` or `no`:")
-
-    if confirm == "yes":
-        fdev.Download(
-            io.BytesIO(bytes.fromhex(data["encryptData"])),
-            len(bytes.fromhex(data["encryptData"])),
-        )
-        time.sleep(0.5)
-        fdev.Oem("unlock")
-
-    else:
-        print("Printing out unlock token.")
-        print(data["encryptData"])
-
-else:
-    print("Done! Printing out unlock token.")
-    print(data["encryptData"])
+print("Done! Printing out unlock token.")
+print(data["encryptData"])

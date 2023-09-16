7548eb799a6848a9c1db803a879d03b1af7f4e41
Jerin Philip <jerinphilip@live.in>
2023-09-16 19:06:57 +0530

    Apply formatting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-import logging, hashlib, io, time
+import hashlib
+import io
+import logging
+import time
+
 from request import Auth, UnlockRequest
 
 logging.basicConfig(level=logging.WARNING)
@@ -10,6 +14,7 @@ use_fastboot = False
 try:
     from adb.fastboot import FastbootCommands
     from adb.usb_exceptions import *
+
     fdev = FastbootCommands()
     try:
         fdev.ConnectDevice()
@@ -23,25 +28,34 @@ auth = Auth()
 auth.login_tui("unlockApi")
 logging.debug(auth.__dict__)
 
-r = UnlockRequest(auth, "unlock.update.miui.com", "/api/v3/unlock/userinfo", {
-  "data":{
-    "uid":auth.userid,
-    "clientId":"1",
-    "clientVersion":"5.5.224.24",
-    "language":"en",
-    "pcId":hashlib.md5(auth.pcid.encode("utf-8")).hexdigest(),
-    "region":"",
-}})
+r = UnlockRequest(
+    auth,
+    "unlock.update.miui.com",
+    "/api/v3/unlock/userinfo",
+    {
+        "data": {
+            "uid": auth.userid,
+            "clientId": "1",
+            "clientVersion": "5.5.224.24",
+            "language": "en",
+            "pcId": hashlib.md5(auth.pcid.encode("utf-8")).hexdigest(),
+            "region": "",
+        }
+    },
+)
 r.add_nonce()
 data = r.run()
 logging.debug(data)
 if not data["shouldApply"]:
-    logging.error("Xiaomi server says shouldApply false, status %d", data["applyStatus"])
+    logging.error(
+        "Xiaomi server says shouldApply false, status %d", data["applyStatus"]
+    )
     input("Press Ctrl-C to cancel, or enter to continue. ")
 
 
-
-mtkqcom = input("What is the SoC brand of the device you want to unlock? `mtk` for Mediatek, `qcom` for Qualcomm: ") # This is the worst way to implement this because i suck at everything
+mtkqcom = input(
+    "What is the SoC brand of the device you want to unlock? `mtk` for Mediatek, `qcom` for Qualcomm: "
+)  # This is the worst way to implement this because i suck at everything
 
 if mtkqcom == "mtk":
     if use_fastboot:
@@ -50,8 +64,12 @@ if mtkqcom == "mtk":
         logging.debug("product is %s, token is %s", product, token)
 
     else:
-        product = input("Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
-        token = input("Enter output from `fastboot oem get_token` \nCombine the strings (left to right) then enter it here. (Ctrl-C to cancel): ")
+        product = input(
+            "Enter output from `fastboot getvar product` (Ctrl-C to cancel): "
+        )
+        token = input(
+            "Enter output from `fastboot oem get_token` \nCombine the strings (left to right) then enter it here. (Ctrl-C to cancel): "
+        )
         logging.debug("product is %s, token is %s", product, token)
 
 
@@ -62,7 +80,9 @@ elif mtkqcom == "qcom":
         logging.debug("product is %s, token is %s", product, token)
 
     else:
-        product = input("Enter output from `fastboot getvar product` (Ctrl-C to cancel): ")
+        product = input(
+            "Enter output from `fastboot getvar product` (Ctrl-C to cancel): "
+        )
         token = input("Enter output from `fastboot getvar token` (Ctrl-C to cancel): ")
         logging.debug("product is %s, token is %s", product, token)
 
@@ -72,41 +92,54 @@ else:
     exit()
 
 
-
-r = UnlockRequest(auth, "unlock.update.miui.com", "/api/v2/unlock/device/clear", {
-  "appId":"1",
-  "data":{
-    "clientId":"1",
-    "clientVersion":"5.5.224.24",
-    "language":"en",
-    "pcId":hashlib.md5(auth.pcid.encode("utf-8")).hexdigest(),
-    "product":product,
-    "region":"",
-}})
+r = UnlockRequest(
+    auth,
+    "unlock.update.miui.com",
+    "/api/v2/unlock/device/clear",
+    {
+        "appId": "1",
+        "data": {
+            "clientId": "1",
+            "clientVersion": "5.5.224.24",
+            "language": "en",
+            "pcId": hashlib.md5(auth.pcid.encode("utf-8")).hexdigest(),
+            "product": product,
+            "region": "",
+        },
+    },
+)
 r.add_nonce()
 data = r.run()
 logging.debug(data)
-print(f"Xiaomi server says: {data['notice']} It says that the unlock will {'' if data['cleanOrNot'] else 'not '}wipe data.")
+print(
+    f"Xiaomi server says: {data['notice']} It says that the unlock will {'' if data['cleanOrNot'] else 'not '}wipe data."
+)
 input("Press Ctrl-C to cancel, or enter to continue. ")
 
-r = UnlockRequest(auth, "unlock.update.miui.com", "/api/v3/ahaUnlock", {
-  "appId":"1",
-  "data":{
-    "clientId":"2", # updated client id
-    "clientVersion":"5.5.224.24", # updated version number
-    "language":"en",
-    "operate":"unlock",
-    "pcId":hashlib.md5(auth.pcid.encode("utf-8")).hexdigest(),
-    "product":product,
-    "region":"",
-    "deviceInfo":{
-      "boardVersion":"",
-      "product":product,
-      "socId":"",
-      "deviceName":""
+r = UnlockRequest(
+    auth,
+    "unlock.update.miui.com",
+    "/api/v3/ahaUnlock",
+    {
+        "appId": "1",
+        "data": {
+            "clientId": "2",  # updated client id
+            "clientVersion": "5.5.224.24",  # updated version number
+            "language": "en",
+            "operate": "unlock",
+            "pcId": hashlib.md5(auth.pcid.encode("utf-8")).hexdigest(),
+            "product": product,
+            "region": "",
+            "deviceInfo": {
+                "boardVersion": "",
+                "product": product,
+                "socId": "",
+                "deviceName": "",
+            },
+            "deviceToken": token,
+        },
     },
-    "deviceToken":token,
-}})
+)
 r.add_nonce()
 data = r.run()
 logging.debug(data)
@@ -115,7 +148,10 @@ if use_fastboot:
     confirm = input("Acquired token! Do you want to unlock now? Enter `yes` or `no`:")
 
     if confirm == "yes":
-        fdev.Download(io.BytesIO(bytes.fromhex(data["encryptData"])), len(bytes.fromhex(data["encryptData"])))
+        fdev.Download(
+            io.BytesIO(bytes.fromhex(data["encryptData"])),
+            len(bytes.fromhex(data["encryptData"])),
+        )
         time.sleep(0.5)
         fdev.Oem("unlock")
 
